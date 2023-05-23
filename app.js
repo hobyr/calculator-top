67b8e55d53dfdcf217f0d8173cfc348625e0ec2e
Hoby Ratefy <hoby.ratefy@protonmail.com>
2023-05-23 22:01:51 +0900

    Add operate function and variables

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
+// Implement basic arithmetic functions
 const add = (num1, num2) => (num1 + num2);
-
 const subtract = (num1, num2) => (num1 - num2);
-
 const multiply = (num1, num2) => (num1 * num2);
-
 const divide = (num1, num2) => (num1 / num2);
 
+let operand1 = 0;
+let operand2 = 0;
+let operator = null;
+
+const operate = (operator, num1, num2) => {
+  const result = 0;
+  switch (operator) {
+    case 'subtract':
+      result = subtract(num1, num2);
+      break;
+    case 'add':
+      result = add(num1, num2);
+      break;
+    case 'multiply':
+      result = multiply(num1, num2);
+      break;
+    case 'divide':
+      result = divide(num1, num2);
+      break;
+  }
+  return result;
+};
